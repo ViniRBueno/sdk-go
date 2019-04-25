ab431db5a22c24b7ad4b3a3f1060954f6bb7f061
Vinicius Ramalho Bueno <vinicius.bueno@ltm.digital>
2019-04-25 11:11:41 -0300

    Creation Templatetype

---
+++ services/templatetype/model.go
@@ -0,0 +1,7 @@
+package template
+
+// TemplateType is the structure of the TemplateType
+type TemplateType struct {
+	ID   int64
+	Name string
+}
---
+++ services/templatetype/templatetype.go
@@ -0,0 +1,44 @@
+package template
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
+	common "github.com/ViniRBueno/sdk-go/services/common"
+)
+
+// Client is the client for Tenant Discovery Rest Api
+type Client struct {
+	common.BaseClient
+}
+
+// NewClient returns a new client configured to communicate on a server with the
+// given hostname and port and to send an Authorization Header with the value of
+// token
+func NewClient(hostname string, token string) *Client {
+
+	return &Client{
+		common.NewBase(
+			hostname,
+			token,
+		)}
+}
+
+// Get gets all the templatetypes
+func (c *Client) Get() (*TemplateType, error) {
+
+	body, err := c.HTTPRequest(fmt.Sprintf("/v1/campaigns/catalogTemplateTypes"), "GET", bytes.Buffer{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	templatetype := &TemplateType{}
+	err = json.NewDecoder(body).Decode(templatetype)
+
+	if err != nil {
+		return nil, err
+	}
+	return templatetype, nil
+}
